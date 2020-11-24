7c2a469995e39e34112636d276f2ef6b9ec5ba5b
Lenny Lord <32369619+thelennylord@users.noreply.github.com>
2020-11-24 17:06:34 +0530

    Make brigadier.py a requirement

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="yamu",
     version="0.1.0",
     py_modules=["yamu"],
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
             "yamu = yamu:main"
